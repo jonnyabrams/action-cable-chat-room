88f297ca62e16d10c5a77ca362c4008d3b43e23f
jonnyabrams <jonnyabrams@hotmail.com>
2023-06-23 15:35:19 +0100

    can post message (as user with random name)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
+
+  def self.generate
+    adjectives = ['Argumentative', 'Bloodyminded', 'Collegial', 'Dastardly', 'Effusive', 'Flawed', 'Gestative']
+
+    nouns = ['Hologram', 'Iguana', 'Joker', 'Koala', 'Leopard', 'Muppet']
+
+    number = rand.to_s[2..4]
+
+    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+
+    create(username: username)
+  end
 end
