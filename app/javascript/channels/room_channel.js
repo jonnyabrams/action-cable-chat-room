9b7765de9cb8b13682aaef13d07a488e44507165
jonnyabrams <jonnyabrams@hotmail.com>
2023-06-24 14:59:56 +0100

    messages send instantly as correct user

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,10 +12,21 @@ consumer.subscriptions.create(
     },
 
     received(data) {
+      const userIdElement = document.getElementById("user-id");
+      const userId = Number(userIdElement.getAttribute("data-user-id"));
+
+      let html;
+
+      if (userId === data.message.user_id) {
+        html = data.mine;
+      } else {
+        html = data.theirs;
+      }
+
       const messageContainer = document.getElementById("messages");
 
       // append new message to existing messages
-      messageContainer.innerHTML = messageContainer.innerHTML + data.html;
+      messageContainer.innerHTML = messageContainer.innerHTML + html;
     },
   }
 );
